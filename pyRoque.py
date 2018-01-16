3651eac82fd458bb6fe73802eda65cb3de447c91
KrypTom143 <mail@wehlan.biz>
2018-01-16 23:27:40 +0100

    Working on user input

--- pyRoque.py
+++ pyRoque.py
@@ -1,4 +1,4 @@
-
+import click
 import re
 from collections import namedtuple
 
@@ -31,20 +31,73 @@ EnvTypes = [
     EnvType("beautiful", 0.0),
     EnvType("narrow", 0.0)
 ]
+class Room:
+    def __init__(self):
+        self.Wall = 0
+        self.Env = 0
+        self.Seed = 0
 
+ 
+
+# Player's location and life
+x = 0
+y = 0
+life = 100
+
+def Look():
+    click.echo("You see nothing")
+
+def TryMoveBy(x,y):
+    return True
+
+def Help():
+    click.echo("Available commands:\n")
+    click.echo("\tl/look\n")
+    click.echo("\tn/s/w/e for moving\n")
+    click.echo("\tquit\n")
+    click.echo("\thelp\n\n")
+    click.echo("Everywhere are Zombies, they come after you.")
+    click.echo("Try to find the helicopter before you are dead.")
+    click.echo("\n\n")
 
 def main():
-    print ("Welcome to the treasure dungeon")
+    click.echo("Welcome to the zombie city, try to find the helicopter to escape.")
     
     # the main loop
-    # Look()
-    while(life > 0) {
+    Look()
+    while(life > 0):
         # Produce the prompt and wait for player's command
-        s = input("%s", life)
+        click.echo('[%s]>' % life)
+        key = click.getchar()
 
-        if (s == "") continue
+        options = { 'w' : TryMoveBy(0,1),
+                    'a' : TryMoveBy(-1,0),
+                    'h' : Help() 
+        }
 
-        if (re.match())
-    }
+        options[key]()
+
+#        if (key == 'h'):
+#            help()
+#        elif (key == 'w'):
+#            if(TryMoveBy(0, -1)):
+#                Look()
+#        elif (key == 'a'):
+#            if(TryMoveBy(-1, 0)):
+#                Look()
+#        elif (key == 's'):
+#            if(TryMoveBy(0, 1)):
+#                Look()
+#        elif (key == 'd'):
+#            if(TryMoveBy(1, 0)):
+#                Look()
+#        elif (key == 'e'):
+#            click.echo('Nothing to eat')
+#        elif (key == 'q'):
+#            break
+#        else:
+#            continue
+
+    click.echo('You have died.')
 
 if __name__ == "__main__": main()
